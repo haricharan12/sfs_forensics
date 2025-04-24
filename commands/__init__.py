1801affccd9838330669c73be1203528be5f6c17
haricharan12 <sharicharan1729@gmail.com>
2025-04-24 18:01:00 -0400

    changes made to main file

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,22 +4,7 @@
 COMMAND_REGISTRY = {}
 
 def register(*names):
-    """
-    Decorator to register a command class in the registry.
-    
-    Args:
-        *names: Optional command names to register. If not provided, 
-                the lowercase name of the class (without 'Command') is used.
-                
-    Example:
-        @register()              # Will register as 'help'
-        class HelpCommand:
-            ...
-            
-        @register('exit', 'quit')  # Will register as both 'exit' and 'quit'
-        class ExitCommand:
-            ...
-    """
+   
     def decorator(cls):
         if not names:
             # Auto-derive name from class name
